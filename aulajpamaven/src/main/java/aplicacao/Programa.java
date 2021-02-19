3c198855b6aef990f9c1ba968e81dc52ed57a33e
Gabriel Rodrigues <gabriel.rodrigues@obuc.com.br>
2021-02-19 18:36:59 -0300

    first commit

---
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -0,0 +1,43 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Pessoa p1= new Pessoa(null, "Carlos da Silva","carlos@gmail.com");
+//		Pessoa p2= new Pessoa(null, "Joaquim Torres","joaquim@gmail.com");
+//		Pessoa p3= new Pessoa(null, "Ana Maria","ana@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+//		em.getTransaction().begin();
+//		em.persist(p1);
+//		em.persist(p2);
+//		em.persist(p3);
+//		em.getTransaction().commit();
+//		
+//		System.out.println(p1);
+//		System.out.println(p2);
+//		System.out.println(p3);
+		
+		
+		
+		//buscando valor do DB
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);
+		
+		System.out.println("Pronto!");
+		
+		em.close();
+		emf.close();
+	}
+
+}
